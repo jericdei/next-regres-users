a014a90ba1ccfd78b4f01c3a7e0dd9f5433d2940
Jeric June Logan <loganjeric25@gmail.com>
2024-07-09 19:08:00 +0800

    remove some utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,14 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export const sleep = (delay: number) =>
-  new Promise((resolve) => setTimeout(resolve, delay));
-
-export function apiUrl(path = "") {
-  const vc = process.env.VERCEL_URL;
-
-  if (vc) return `https://${vc}/api/${path}`;
-
-  return `http://localhost:3000/api/${path}`;
-}
